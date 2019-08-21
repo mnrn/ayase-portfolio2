721a1f7c4e29b98c04ec5de26f6149c60f68349d
ayase <paper.airplane01@gmail.com>
2019-08-21 13:39:48 +0900

    Transport NotFound.vue and KVTable.vue

---
+++ src/components/layouts/KVTable.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div#kvtable
+    table
+      thead
+        tr
+          th Key
+          th Value
+      tbody
+        tr(v-for="(item, index) in items" :key="index")
+          th(v-cloak) {{item.key}}
+          td(v-if="'url' in item")
+            a(:href="item.url" target="_blank" rel="noopener") {{item.value}}
+          td(v-else v-cloak) {{item.value}}
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import about from '@/assets/data/about.json'
+import skills from '@/assets/data/skills.json'
+
+export type About = typeof about
+export type Skills = typeof skills
+
+@Component
+export default class KVTable extends Vue {
+  @Prop() private items!: (About | Skills)
+}
+</script>
+
+<style lang="stylus" scoped>
+#kvtable
+  display: table
+  font-weight: bold
+  width: auto
+  margin: 50px auto
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  div#not-found
+    main
+      h1 ページが見つかりません
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'// @ is an alias to /src
+
+@Component
+export default class NotFound extends Vue {}
+</script>
