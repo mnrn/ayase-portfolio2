9a22f1dda3539a50f0da58f31cd8c8aeb614d98e
ayase <paper.airplane01@gmail.com>
2019-08-22 22:48:40 +0900

    Transport Header, Footer file

---
+++ src/components/FooterNav.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div#footer-nav
+    font-awesome-layers(class="fa-3x fa-fw")
+      a(href="https://twitter.com/localhost5050" target="_blank" class="sns-link" ref="noopener")
+        font-awesome-icon(:icon="twitter")
+      a(href="https://github.com/mnrn" target="_blank" class="sns-link" ref="noopener")
+        font-awesome-icon(:icon="github")
+      a(href="https://bookmeter.com/users/1017368" target="_blank" class="sns-link" ref="noopener")
+        font-awesome-icon(:icon="book")
+    div.copyright-box © 2019 あやせさん
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+@Component({
+  components: {
+    FontAwesomeIcon
+  }
+})
+export default class FooterNav extends Vue {
+  private twitter = faTwitter
+  private github = faGithub
+  private book = faBook
+}
+</script>
+
+<style lang="stylus" scoped>
+#footer-nav
+  text-align: center
+  margin-top: 100px
+  .copyright-box
+    font-size: 11px
+</style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,35 @@
+
+<template lang="pug">
+  nav#header-nav
+    ul
+      li
+        a(href="#" v-scroll-to="'#header-nav'") Home
+      li
+        a(href="#" v-scroll-to="'#about'") About
+      li
+        a(href="#" v-scroll-to="'#skills'") Skills
+      li
+        a(href="#" v-scroll-to="'#works'") Works
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeaderNav extends Vue {}
+</script>
+
+<style lang="stylus" scoped>
+#header-nav
+  font-weight: bold
+  position: fixed
+  top: 0
+  right: 25px
+  font-size: 18px
+  z-index: 1000
+  ul
+    li
+      display: inline-block
+      margin: 0 5px
+      padding: 0 5px
+</style>
