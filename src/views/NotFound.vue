18e1b7992f4d0b2faec268692d5937012ca38752
ayase <paper.airplane01@gmail.com>
2020-06-10 20:45:48 +0900

    Update Not Found

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   div#not-found
+    header
+      h1 Oops!
     main
-      h1 ページが見つかりません
+      img(:src="require('@/assets/img/page-not-found.webp')")
 </template>
 
 <script lang="ts">
@@ -10,3 +12,8 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class NotFound extends Vue {}
 </script>
+
+<style lang="stylus" scoped>
+#not-found
+  text-align: center
+</style>
