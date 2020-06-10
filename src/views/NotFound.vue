6669d43695bff127a5d59d083e17c9026bc6fd33
ayase <paper.airplane01@gmail.com>
2020-06-10 22:52:24 +0900

    Finish making Not Found Page.

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,6 @@
 <template lang="pug">
-  div#not-found
-    header
-      h1 Oops!
-    main
+  div#not-found(:style="style")
+    div.not-found-image
       img(:src="require('@/assets/img/page-not-found.webp')")
 </template>
 
@@ -10,10 +8,26 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class NotFound extends Vue {}
+export default class NotFound extends Vue {
+  private style = 'background-image: url(' + require('@/assets/img/bg404.webp') + ')'
+}
 </script>
 
 <style lang="stylus" scoped>
 #not-found
-  text-align: center
+  min-height: 100%
+  background-position: center
+  background-repeat: no-repeat
+  background-size: cover
+  height: 100%
+  margin: 0
+  padding: 0
+  .not-found-image
+    text-align: center
+    img
+      padding: 0
+      display: block
+      margin: 0 auto
+      max-height: 100%
+      max-width: 100%
 </style>
