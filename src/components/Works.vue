ebc8c31ee59c8c491bc99de24bf537615844c2a7
ayase <paper.airplane01@gmail.com>
2020-09-30 11:59:38 +0900

    Embed responsive.

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -4,12 +4,12 @@ div#works
   headline(text="Works")
   div.card-deck(class="d-flex justify-content-center align-items-center")
     div.card(v-for="work in works" :key="work.id")
-      div.card-img(class="border-bottom d-flex justify-content-center align-items-center flex-wrap flex-sm-nowrap")
+      div.card-img(class="border-bottom d-flex justify-content-center align-items-center flex-wrap flex-sm-nowrap embed-responsive embed-responsive-16by9")
         a.card-link(:href="work.url !== '' ? work.url : work.repo" target="_blank" rel="noopener")
-          img(v-if="work.pic.startsWith('https')" :src="work.pic" alt="My work image." height="300px" width="auto")
+          img(v-if="work.pic.startsWith('https')" :src="work.pic" alt="My work image.")
           picture(v-else)
             source(type="image/webp" :srcset="require('@/assets/img/' + work.pic + '.webp')")
-            img.card-img-top(:src="require('@/assets/img/' + work.pic + '.png')" alt="My work image." height="300px" width="auto")
+            img.card-img-top(:src="require('@/assets/img/' + work.pic + '.png')" alt="My work image.")
       div.card-body
         p.card-title(v-cloak) {{work.title}}
         div(v-for="(text, index) in work.texts" :key="index")
@@ -45,7 +45,9 @@ export default class Works extends Vue {
     .card-body
       min-height: 170px
     .card-img
-      height: 350px
+      width: 100%;
+      height: 40vh;
+      object-fit: cover;
     .card-title
       font-size: 20px
       font-weight: bold
