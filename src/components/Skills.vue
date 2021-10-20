fb38a72205f76baef255f6cae87dedb322c671ac
ayase <paper.airplane01@gmail.com>
2021-10-20 21:10:21 +0900

    Update skills.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,7 +4,7 @@
     div.main
       div.card-deck(class="d-flex justify-content-center align-items-start")
         skill-subroot(title="プログラミング言語" :subroot="langs")
-        skill-subroot(title="開発サービス" :subroot="servs")
+        skill-subroot(title="ソフトウェアサービス" :subroot="saas")
         skill-subroot(title="OS" :subroot="os")
         skill-subroot(title="その他" :subroot="etc")
 </template>
@@ -12,13 +12,12 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import Headline from '@/components/layouts/Headline.vue'
-import PairsTable from '@/components/layouts/PairsTable.vue'
 import SkillSubroot from '@/components/layouts/SkillSubroot.vue'
 
 import { SkillSubtree } from '@/static/ts/skill'
 import langs from '@/assets/data/ja/skill-langs.json'
 import os from '@/assets/data/ja/skill-os.json'
-import servs from '@/assets/data/ja/skill-servs.json'
+import saas from '@/assets/data/ja/skill-saas.json'
 import etc from '@/assets/data/ja/skill-etc.json'
 
 @Component({
@@ -30,7 +29,7 @@ import etc from '@/assets/data/ja/skill-etc.json'
 export default class Skills extends Vue {
   private langs: SkillSubtree = langs
   private os: SkillSubtree = os
-  private servs: SkillSubtree = servs
+  private saas: SkillSubtree = saas
   private etc: SkillSubtree = etc
 }
 </script>
